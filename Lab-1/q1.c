fddcb6c494c52bedc8d44035d6801ffdfa0736a8
abhirupchakraborty1 <72644463+abhirupchakraborty1@users.noreply.github.com>
2021-10-07 13:43:17 +0530

    Update q1.c

--- Lab-1/q1.c
+++ Lab-1/q1.c
@@ -1,34 +1,34 @@
-#include<stdio.h>
+#include<stdio.h>//Importing the standard input output header file.
 
-int main()
+int main()//main function.
 {
     int n, i, s, l;
     printf("Enter number of elements\n");
-    scanf("%d",&n);
+    scanf("%d",&n);//Accepting the limit of the array from user.
     int a[n];
     printf("Array:\n");
     for(i=0;i<n;i++)
     {
-        a[i]=rand();
-        printf("%d\t",a[i]);
+        a[i]=rand();//Accepting random input.
+        printf("%d\t",a[i]);//Printing the array element.
     }
 
     printf("\n");
 
-    s=l=a[0];
+    s=l=a[0];//Intialising the smallest and largest value storing variable with the 1st element of the array.
     for(i=0;i<n;i++)
     {
-        if(a[i] < s)
+        if(a[i] < s)//if the current element of the array is less than the presently calculated smallest value.
         {
             s=a[i];
         }
-        else if(a[i] > l)
+        else if(a[i] > l)//if the current element of the array is greater than the presently calculated smallest value.
         {
             l=a[i];
         }
     }
 
-    printf("Smallest number = %d \nLargest number = %d\n", s, l);
+    printf("Smallest number = %d \nLargest number = %d\n", s, l);//Printing the smallest and largest elements of the array.
 
     return 0;
 }
