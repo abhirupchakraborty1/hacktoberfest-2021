a156ff89ef154dac75bc2da79a45213f6d21857f
abhirupchakraborty1 <72644463+abhirupchakraborty1@users.noreply.github.com>
2021-10-07 13:52:17 +0530

    Update q4.c

--- Lab-1/q4.c
+++ Lab-1/q4.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include<stdio.h>//Importing the header file for standard input and output operations.
 
-void SWAP(int *x, int *y)
+void SWAP(int *x, int *y)//The swap function to swap the values which takes 2 parameters.
 {
     *x = *x + *y;
     *y = *x - *y;
     *x = *x - *y;
 }
 
-int main()
+int main()//The main function
 {
     int a, b, c;
     printf("Enter:\na  b  c\n");
-    scanf("%d%d%d",&a,&b,&c);
-    SWAP(&a, &b);
-    SWAP(&a, &c);
-    printf("Values after rotating are: \na\tb\tc \n%d\t%d\t%d\n", a, b, c);
+    scanf("%d%d%d",&a,&b,&c);//Accepting 3 numbers from the user.
+    SWAP(&a, &b);//Swapping the values of a and b.
+    SWAP(&a, &c);//Swapping the values of a and c.
+    printf("Values after rotating are: \na\tb\tc \n%d\t%d\t%d\n", a, b, c);//Printing the final values after rotation.
     return 0;
 }
